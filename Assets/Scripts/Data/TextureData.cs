4b3c66de6f99ce25dff81a59cfad1361d0082769
PaburoTC <pablosky301@gmail.com>
2022-05-03 11:32:23 -0400

    Final commit

--- Assets/Scripts/Data/TextureData.cs
+++ Assets/Scripts/Data/TextureData.cs
@@ -16,12 +16,12 @@ public class TextureData : ScriptableObject {
 
 	public void ApplyToMaterial(Material material) {
 		
-		material.SetInt ("layerCount", layers.Length);
-		material.SetColorArray ("baseColors", layers.Select(x => x.color).ToArray());
-		material.SetFloatArray ("baseStartHeights", layers.Select(x => x.startHeight).ToArray());
-		material.SetFloatArray ("baseBlends", layers.Select(x => x.blendStrength).ToArray());
-		material.SetFloatArray ("baseColorStrength", layers.Select(x => x.colorStrength).ToArray());
-		material.SetFloatArray ("baseTextureScales", layers.Select(x => x.textureScale).ToArray());
+		material.SetInt("layerCount", layers.Length);
+		material.SetColorArray("baseColors", layers.Select(x => x.color).ToArray());
+		material.SetFloatArray("baseStartHeights", layers.Select(x => x.startHeight).ToArray());
+		material.SetFloatArray("baseBlends", layers.Select(x => x.blendStrength).ToArray());
+		material.SetFloatArray("baseColorStrength", layers.Select(x => x.colorStrength).ToArray());
+		material.SetFloatArray("baseTextureScales", layers.Select(x => x.textureScale).ToArray());
 		Texture2DArray texturesArray = GenerateTextureArray (layers.Select (x => x.texture).ToArray ());
 		material.SetTexture ("baseTextures", texturesArray);
 
